feec6627fbe682f8a8bfc1c16f5f521cbae9a9ad
Gulnara Mirzakarimova <gulnara@gulnara.me>
2014-10-16 13:13:26 -0700

    added console and started test file

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'mongo'
 require 'mongo_mapper'
 require 'uri'
+require 'logbert'
 require 'json'
 
 mongo_url = ENV['MONGOHQ_URL'] || 'mongodb://localhost/dbname'
@@ -10,37 +11,43 @@ MongoMapper.connection = Mongo::Connection.from_uri mongo_url
 MongoMapper.database = URI.parse(mongo_url).path.gsub(/^\//, '') #Extracts 'dbname' from the uri
 require './models/vault'
 
+Logbert.root.handlers << Logbert::Handlers::StreamHandler.new
 LOG = Logbert[self]
 
-post '/parse' do
+post '/event' do
+	LOG.info params
+	puts params
 	content_type :json
-  register(params).to_json
+	json = JSON.parse(request.body.read)
+	which_type(json).to_json
 end
 
 def which_type(event)
-	if event[:type] == "enter" or type == "leave"
+
+	LOG.info "here is my event #{event}"
+	if event["type"] == "enter" or event["type"] == "leave"
 		enter_leave(event)
-	elsif event[:type] == "comment"
+	elsif event["type"] == "comment"
 		comment(event)
-	elsif event[:type] == "highfive"
+	elsif event["type"] == "highfive"
 		high_five(event)
 	else
-		LOG.error "Oh oh, this is an indentified event!"
-		raise "Oh oh, this is an indentified event!"
+		LOG.error "Oh oh, this is an unidentified event! #{event}"
+		raise "Oh oh, this is an unidentified event! #{event}"
 	end
 end	
 
 def enter_leave(event)
-	event = Vault.new(:date => event["date"], :user => event["user"], :type => event["type"])
+	event = Vault.new(:date => Time.new(event["date"]), :user => event["user"], :type => event["type"])
 	event.save
 end
 
 def comment(event)
-	event = Comments.new(:date => event["date"], :user => event["user"], :type => event["type"], :message => event["message"])
+	event = Comments.new(:date => Time.new(event["date"]), :user => event["user"], :type => event["type"], :message => event["message"])
 	event.save
 end
 
 def high_five(event)
-	event = HighFives.new(:date => event["date"], :user => event["user"], :type => event["type"], :otheruser => event["otheruser"])
+	event = HighFives.new(:date => Time.new(event["date"]), :user => event["user"], :type => event["type"], :otheruser => event["otheruser"])
 	event.save
 end
