5e074f4a80055a2a880c5d26058c7faa1384c820
Gulnara Mirzakarimova <gulnara@gulnara.me>
2014-10-17 08:44:12 -0700

    fixed time parsing

--- app.rb
+++ app.rb
@@ -12,6 +12,8 @@ MongoMapper.database = URI.parse(mongo_url).path.gsub(/^\//, '') #Extracts 'dbna
 require './models/vault'
 
 Logbert.root.handlers << Logbert::Handlers::StreamHandler.new
+Logbert.root.handlers << Logbert::Handlers::StreamHandler.for_path("logbert.log")
+Logbert.root.level = :info
 LOG = Logbert[self]
 
 post '/event' do
@@ -21,7 +23,7 @@ post '/event' do
 	which_type(json).to_json
 end
 
-get '/events?from=:from&to=:to' do
+get '/events' do
 	from = params[:from] 
 	to = params[:to]
 	get_events(from, to)
@@ -29,16 +31,27 @@ end
 
 def get_events(from, to)
 	events = Vault.all
+	new_from = parse_date(from)
+	new_to = parse_date(to)
 	all_events = []
 	events.each do |e|
-		if e.date >= from or e.date <= to
-			all_events << e
+		date = e["date"]
+		if date >= new_from and date <= new_to
+			event = e.attributes
+			all_events << event.reject{|k,v| k == "_type" or k =="_id" }.to_json
+		else
+			LOG.info "No events were found"
 		end
 	end
 	return all_events
 end
 
-get '/summary?from=:from&to=to&by=:by' do
+def parse_date(date)
+	date = Time.strptime(date, "%Y-%m-%dT%H:%M:%S%z")
+	return date
+end
+
+get '/summary' do
 	from = params[:from] 
 	to = params[:to]
 	by = params[:by]
@@ -48,29 +61,66 @@ end
 def get_summary(from, to, by)
 	events = Vault.all
 	all_events = []
-	new_from = parse_date(from, by)
-	new_to = parse_date(to, by)
-	events.each do |e|
-		if e.date >= new_from or e.date <= new_to
-			all_events << e
-		end
-	end
-	return all_events
-end
-
-def parse_date(date,by)
 	if by == "minute"
-
+		events.each do |e|
+			time = trim_minute(e["date"])
+			add_event(time, new_from, new_to, all_events)
+		end
 	elsif by == "hour"
-
-	elsif by == "hour"
-
+		events.each do |e|
+			time = trim_hour(e["date"])
+			add_event(time, new_from, new_to, all_events)
+		end
+	elsif by == "day"
+		events.each do |e|
+			time = trim_day(e["date"])
+			add_event(time, new_from, new_to, all_events)
+		end
 	else
 		LOG.error "Please specify the timeframe as : minute, hour or day"
 		raise "Please specify the timeframe as : minute, hour or day"
 	end
 
+	return all_events
+
 end
+
+def count(events)
+	enters = events.select{|a| a["type"]=="enter"}.count
+	leaves = events.select{|a| a["type"]=="leave"}.count
+	comments = events.select{|a| a["type"]=="comment"}.count
+	highfives = events.select{|a| a["type"]=="highfive"}.count
+end
+
+def summary(from, to, by)
+	events = get_summary(from, to, by)
+	
+
+def add_event(time, new_from, new_to, all_events)
+	if time >= new_from or time <= new_to
+		event = e.attributes
+		all_events << event.reject{|k,v| k == "_type" or k =="_id" }.to_json
+	end
+end	
+
+def trim_minute(time)
+	p_time = parse(time)
+	t_time = p_time.strftime("%Y-%m-%dT%H:%M%Z")
+	new_time = Time.strptime(t_time, "%Y-%m-%dT%H:%M%Z")
+end
+
+def trim_hour(time)
+	p_time = parse(time)
+	t_time = p_time.strftime("%Y-%m-%dT%H%Z")
+	new_time = Time.strptime(t_time, "%Y-%m-%dT%H%Z")
+end
+
+def trim_date(time)
+	p_time = parse(time)
+	t_time = p_time.strftime("%Y-%m-%d")
+	new_time = Time.strptime(t_time, "%Y-%m-%d")
+end
+
 
 
 def which_type(event)
@@ -89,16 +139,16 @@ def which_type(event)
 end	
 
 def enter_leave(event)
-	event = Vault.new(:date => Time.new(event["date"]), :user => event["user"], :type => event["type"])
+	event = Vault.new(:date => Time.strptime(event["date"], "%Y-%m-%dT%H:%M:%S%z"), :user => event["user"], :type => event["type"])
 	event.save
 end
 
 def comment(event)
-	event = Comments.new(:date => Time.new(event["date"]), :user => event["user"], :type => event["type"], :message => event["message"])
+	event = Comments.new(:date => Time.strptime(event["date"], "%Y-%m-%dT%H:%M:%S%z"), :user => event["user"], :type => event["type"], :message => event["message"])
 	event.save
 end
 
 def high_five(event)
-	event = HighFives.new(:date => Time.new(event["date"]), :user => event["user"], :type => event["type"], :otheruser => event["otheruser"])
+	event = HighFives.new(:date => Time.strptime(event["date"], "%Y-%m-%dT%H:%M:%S%z"), :user => event["user"], :type => event["type"], :otheruser => event["otheruser"])
 	event.save
 end
